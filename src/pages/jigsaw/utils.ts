749b0b5e88be031bcffebe774097238a492e2575
ratat <ratatatat1@163.com>
2025-06-06 11:21:47 +0800

    test bugbot

--- src/pages/jigsaw/utils.ts
+++ src/pages/jigsaw/utils.ts
@@ -1,3 +1,7 @@
 export const getChangeIndex = (offsetX: number, offsetY: number) => {
   return ((offsetY / 100) | 0) * 3 + ((offsetX / 100) | 0);
 };
+export const getPackageName = () => {
+  const packageName = localStorage.getItem("packageName") || "jigsaw-package";
+  return packageName;
+};
